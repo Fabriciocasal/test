1b5848aaa222b4be85e97d65d255ba85d393fbf1
alumno <alumno>
2017-08-17 17:53:26 -0300

    Final de la Clase 1

---
+++ src/Prueba.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		// TODO Auto-generated method stub
+        String numeroUno; 
+        String numeroDos;
+     
+        //System.out.println("el resultado es " + resultado);
+        
+        String nombreyapellido;
+        Scanner scanner  = new Scanner(System.in); 
+        
+        System.out.println("ingrese un numero");
+        numeroUno = scanner.nextLine();
+
+        System.out.println("ingrese un numero");
+        numeroDos = scanner.nextLine();
+
+        String numeroUno1 = scanner.nextLine()
+        		
+        String resultado=numeroUno1+numeroDos;
+       //resultado = scanner.nextLine();
+       
+        System.out.println("el resultado es "+ resultado);
+        
+        Integer.parseInt(scanner);
+        
+	}
+
+}
