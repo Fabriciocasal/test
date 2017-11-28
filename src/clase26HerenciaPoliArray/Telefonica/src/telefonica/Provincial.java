f2664ec1fe608f984b51c848760d2cb43d55ea6d
Fabriciocasal <31107807+Fabriciocasal@users.noreply.github.com>
2017-11-28 17:55:35 -0300

    clase26

---
+++ src/clase26HerenciaPoliArray/Telefonica/src/telefonica/Provincial.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telefonica;
+
+/**
+ *
+ * @author alumno
+ */
+public class Provincial extends Llamada{
+    
+    public Provincial(float _duracion, String _nroDestino, String _nroOrigen) {
+        super(_duracion, _nroDestino, _nroOrigen);
+    }
+    
+}
